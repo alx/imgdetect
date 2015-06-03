6201f6fc9cbe73834b5e2d1b4311cb4a63a77e13
Alexandre Girard <git@alexgirard.com>
2015-06-03 12:21:40 +0200

    change js to use progressbar

--- public/js/main.js
+++ public/js/main.js
@@ -10,9 +10,24 @@ $(document).ready(function() {
       $('#emptyImage img').attr('src', data.url);
       $('#emptyImage ul').html('');
       $.each(data.predictions, function(prediction) {
-        $('#emptyImage ul').append(
-          '<li><a href="#">' + this.name + '</a> - ' + (this.prob * 100).toFixed(1) + '%</li>'
-        );
+        var percent = parseInt(this.prob * 100);
+
+        var style = 'success';
+
+        if(percent < 60) {
+          style='warning';
+        }
+
+        if(percent < 25) {
+          style = 'danger';
+        }
+
+        var predictionHtml = '<div class="row"><div class="col-lg-4"><div class="progress">';'
+        predictionHtml += '<div class="progress-bar progress-bar-' + style + '" role="progressbar" ';
+        predictionHtml += 'aria-valuenow="' + percent + '" aria-valuemin="0" aria-valuemax="100" ';
+        predictionHtml += 'style="width: ' + percent + '%;">' + percent + '%</div></div></div>';
+        predictionHtml += '<div class="col-lg-8"><a href="#">' + this.name + '</a></div></div>';
+        $('#emptyImage .predictions').append(predictionHtml);
       });
 
       $('#imageList').prepend('<hr>');
