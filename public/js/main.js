b85ae6eb2693fb79a49e202fdcd529326bebb43f
Alexandre Girard <git@alexgirard.com>
2015-06-03 10:00:15 +0200

    add missing js/images

---
+++ public/js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+  $('a#urlSubmit').click(function() {
+
+    $('.loading').removeClass('hidden');
+
+    $.post('/api/images', {url: $('input#url').val()})
+    .done(function(data) {
+      data = JSON.parse(data);
+      $('#emptyImage img').attr('src', data.url);
+      $('#emptyImage ul').html('');
+      $.each(data.predictions, function(prediction) {
+        $('#emptyImage ul').append(
+          '<li><a href="#">' + this.name + '</a> - ' + (this.prob * 100).toFixed(1) + '%</li>'
+        );
+      });
+
+      $('#imageList').prepend('<hr>');
+      $('#imageList').prepend($('#emptyImage').clone().attr('id', '').removeClass('hidden'));
+      $('.loading').addClass('hidden');
+    });
+
+  });
+
+});
